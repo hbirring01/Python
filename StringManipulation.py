228da25bdd3880e7c1fbae2b514d5c269d5aca15
hbirring01 <hbirring01@gmail.com>
2018-12-04 21:56:03 -0500

    Adding contents to string manipulation

--- StringManipulation.py
+++ StringManipulation.py
@@ -1,1 +1,23 @@
 # I will be manipulating strings using python in this file!
+string = "Hello World"
+
+#Print the first character from the string
+print(string[0])
+
+#Print the whole string
+print(string[:])
+
+#grab the letter l
+#This goes backwards since its negative. You are grabbing the l from the world
+print(string[-2])
+print(string[2])
+
+#print the first 3
+print(string[:3])
+
+#grab a subsection. The last number is not included so you go one more than what is required
+print(string[0:5])
+
+#grab everything in the string by 2's
+print(string[::2])
+print(string[0:6:2])
