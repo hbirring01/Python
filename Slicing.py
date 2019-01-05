c985a768fa4611f085033e4d97fcc506be245a9b
hbirring01 <hbirring01@gmail.com>
2019-01-05 00:33:15 -0500

    adding more info to recap

---
+++ Slicing.py
@@ -0,0 +1,13 @@
+word = 'supercalifragilisticexpialidocious'
+
+print(word[word.index('fragi'):word.index('expi')])
+
+#output is fragilistic
+
+#Recap
+"""
+strings are iterable and immutable. We can access the individual elements in a string
+
+The basic slice format is:
+variable[start:end:step]
+"""
