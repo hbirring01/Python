412ab873479094f558217a5cb339a40b97fca7f9
hbirring01 <hbirring01@gmail.com>
2019-01-05 00:15:00 -0500

    updated documentation

---
+++ Slicing.py
@@ -0,0 +1,12 @@
+word = 'supercalifragilisticexpialidocious'
+
+print(word[word.index('fragi'):word.index('expi')])
+
+#output is fragilistic
+
+#Recap
+"""
+strings are iterable and immutable. We can access the individual elements in a string
+
+
+"""
