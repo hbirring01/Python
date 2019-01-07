88532ffe985fb4dc2f08ee34819b7c6992f1df49
hbirring01 <hbirring01@gmail.com>
2019-01-07 17:09:49 -0500

    Notes on Logic Operators

---
+++ Logic-Operators.py
@@ -0,0 +1,23 @@
+#Logic
+
+#Boolean is a data type that has only true or false value
+
+B = True
+C = False 
+
+#Booleans can not be all lower case, They must start with an uppercase letter (T, F). Also, you cannot use quotes 
+#Booleans are created by doing a compare operation
+
+2 < 3 # 2 is less than 3
+
+2 == 3 # 2 is equal to 3
+
+3 < 2 # 3 is less than 2 
+
+2 != 3  # 2 is not equal to 3
+
+4 >= 2 # 4 is geater than or equal to 2
+
+3 <=2 # 3 is less than or equal to 2 
+
+
