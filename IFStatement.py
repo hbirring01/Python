b0dda3de838280a1c3875d5bd40f79daca7e33c9
hbirring01 <hbirring01@gmail.com>
2019-01-07 17:58:45 -0500

    Added more notes

---
+++ IFStatement.py
@@ -0,0 +1,21 @@
+#If statement
+#If condition:
+#   execute code
+
+if True:
+    print('it worked!')
+
+#Else if statement
+"""
+if condition 1:
+    code
+elif condition 2: 
+    code
+elif condition 3:
+    code :
+else:
+    code 
+
+
+
+"""
