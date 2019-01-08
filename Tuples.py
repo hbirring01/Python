4648120d2e17cc0c31678d8a69be5bdb0d23d859
hbirring01 <hbirring01@gmail.com>
2019-01-08 17:58:07 -0500

    Notes on tuples

---
+++ Tuples.py
@@ -0,0 +1,15 @@
+#Tuples are similar to lists but they can not be changed (immutable), they are made of elements like lists so all element operations are valid, 
+
+my_tuple = (1,2,3,"a","b","c")
+
+my_list = [1,2,3,4,5,6,7]
+
+my_list[2] = 100
+
+#you cant do that with a tuple
+
+#When you want to use a tuple is if you want data to be protected but refer to it later. You dont want it to be modified
+
+#Use the tuple() function to convert other things to a tuple 
+
+
