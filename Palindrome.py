d12f5acf74cc0cfb11b3d2babd4baae59da15b3d
hbirring <hbirring01@gmail.com>
2019-01-16 21:16:41 -0500

    Rewrote for the fun of it

--- Palindrome.py
+++ Palindrome.py
@@ -2,7 +2,7 @@ tocheck = input('Enter a word to see if it is a palindrome: ').strip().lower()
 
 reverse = tocheck[::-1]
 
-if reverse == tocheck:
-    print('{} is a palindrome'.format(tocheck))
+if tocheck == reverse:
+    print('{} is a palindrome!'.format(tocheck))
 else:
-    print('{} is not a palindrome'.format(tocheck))
+    print('{} is not a palindrome!'.format(tocheck))
